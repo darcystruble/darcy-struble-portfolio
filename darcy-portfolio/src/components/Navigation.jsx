964c128d7d9649507da4ff7d7b8d27142454d3e3
darcystruble <darcyhallstruble@gmail.com>
2024-01-10 08:23:54 -0500

    updates to style, simplifying the overall look

--- darcy-portfolio/src/components/Navigation.jsx
+++ darcy-portfolio/src/components/Navigation.jsx
@@ -5,13 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export default function Navigation() {
     return (
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark-subtle" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Darcy Struble</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">// Home</Nav.Link>
+            <Nav.Link href="#about">// About</Nav.Link>
+            <Nav.Link href="#projects">// Projects</Nav.Link>
+            <Nav.Link href="#contact">// Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
