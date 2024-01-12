2379774cebbaf47856565385bb502a95ecfce64a
darcystruble <darcyhallstruble@gmail.com>
2024-01-12 11:44:32 -0500

    styling is mostly in place, content needs to be finished

--- darcy-portfolio/src/components/elements/About.jsx
+++ darcy-portfolio/src/components/elements/About.jsx
@@ -1,14 +1,24 @@
+// image
 import darcyPic from '../../assets/darcy.jpg'
+// from react
+import { useEffect } from 'react'
+// AOS
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 export default function About() {
+    useEffect(()=>{
+        AOS.init()
+    }, [])
+
     return (
         <div className="about-outer section section-bg">
             <h2>About me...</h2>
             <div id="about">
-                <div className='img-div'>
+                <div className='img-div' data-aos="flip-left" data-aos-duration="500">
                     <img src={darcyPic} alt="" />
                 </div>
-                <div className="text">
+                <div className="text" data-aos="fade-left" data-aos-duration="2000">
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, soluta explicabo consequuntur modi vitae omnis reiciendis debitis. Quasi minus doloremque odio magni. Ducimus nobis repellendus, recusandae minima sed ut cumque?</p>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque quo tempora nisi sunt saepe. Voluptatem soluta, deleniti dolorum quo, aliquid velit architecto unde aut optio nostrum, quos perspiciatis minima. Eveniet.</p>
                 </div>
--- darcy-portfolio/src/components/elements/Contact.jsx
+++ darcy-portfolio/src/components/elements/Contact.jsx
@@ -3,6 +3,10 @@ export default function Contact() {
     return (
         <div id="contact" className="section section-bg">
             <h2>Contact</h2>
+            <button><a>Github</a></button>
+            <button><a>Linkedin</a></button>
+            <button><a>Email</a></button>
+            <button><a>Resume</a></button>
         </div>
     )
 }
