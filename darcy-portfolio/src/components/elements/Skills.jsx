fd3a46952ced025524b71aa99f303c9b654d0f83
darcystruble <darcyhallstruble@gmail.com>
2024-01-11 23:35:16 -0500

    basic setup is mostly done, need to finish contact set up and footer

--- darcy-portfolio/src/components/elements/Skills.jsx
+++ darcy-portfolio/src/components/elements/Skills.jsx
@@ -1,6 +1,79 @@
+// css
+import '../styles/Skills.css'
+// icon imports
+// languages
+import { JavascriptPlain } from 'devicons-react'
+import { TypescriptPlain } from 'devicons-react'
+import { PythonPlain } from 'devicons-react'
+import { Css3PlainWordmark } from 'devicons-react'
+import { Html5PlainWordmark } from 'devicons-react'
+// frameworks
+import { ReactOriginal } from 'devicons-react'
+import { BootstrapPlain } from 'devicons-react'
+import { ExpressOriginal } from 'devicons-react'
+// databases
+import { MongodbPlain } from 'devicons-react'
+import { DjangoPlain } from 'devicons-react'
 
 export default function Skills() {
     return (
-        <div id="about">Skills</div>
+        <div id="skills">
+            <h2>Skills</h2>
+            <div className="langs">
+                <h3>Languages</h3>
+                <div className="group">
+                    <div className="icon">
+                        <JavascriptPlain color="linen" size="70"/>
+                        <div>JavaScript</div>
+                    </div>
+                    <div className="icon">
+                        <PythonPlain color="oldlace" size="70"/>
+                        <div>Python</div>
+                    </div>
+                    <div className="icon">
+                        <TypescriptPlain color="oldlace" size="70"/>
+                        <div>TypeScript</div>
+                    </div>
+                    <div className="icon">
+                        <Css3PlainWordmark color="oldlace" size="70"/>
+                        <div>CSS3</div>
+                    </div>
+                    <div className="icon">
+                        <Html5PlainWordmark color="oldlace" size="70"/>
+                        <div>HTML5</div>
+                    </div>
+                </div>
+            </div>
+            <div className="frames">
+                <h3>Frameworks</h3>
+                <div className="group">
+                    <div className="icon">
+                        <ReactOriginal size="70"/>
+                        <div>React.js</div>
+                    </div>
+                    <div className="icon">
+                        <BootstrapPlain color="oldlace" size="70"/>
+                        <div>Bootstrap</div>
+                    </div>
+                    <div className="icon">
+                        <ExpressOriginal size="70"/>
+                        <div>Express.js</div>
+                    </div>
+                </div>
+            </div>
+            <div className="dbs">
+                <h3>Databases</h3>
+                <div className="group">
+                    <div className="icon">
+                        <MongodbPlain size="70"/>
+                        <div>MongoDB</div>
+                    </div>
+                    <div className="icon">
+                        <DjangoPlain size="70"/>
+                        <div>Django</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
